98e0425f260aba4f3a9c63d242e403d2f63ac857
wi22b070 <wi22b070@technikum-wien.at>
2023-06-24 23:07:31 +0200

    initial commit

---
+++ src/test/java/com/example/slm_exam_file/SlmExamFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
