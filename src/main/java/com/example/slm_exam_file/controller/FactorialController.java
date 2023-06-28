5e25daacd236291921e87519cbfbb7063fa685e0
wi22b070 <wi22b070@technikum-wien.at>
2023-06-28 18:46:48 +0200

    refactored previous method to implement running total, implementing running total method + corresponding tests + deleted unused imports

--- src/main/java/com/example/slm_exam_file/controller/FactorialController.java
+++ src/main/java/com/example/slm_exam_file/controller/FactorialController.java
@@ -1,6 +1,6 @@
 package com.example.slm_exam_file.controller;
 
-import org.springframework.web.bind.annotation.RequestAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
